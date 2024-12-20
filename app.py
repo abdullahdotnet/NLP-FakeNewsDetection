2e41c9c60b691dfd921cc18a17ac56f63e544527
abdullah <abdullahdotnet20@gmail.com>
2024-12-20 21:59:38 +0500

    docker implementation with logging in sql database

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import pickle
+import nltk
+import time
+import pymysql
+from datetime import datetime
+
+nltk.download('stopwords')
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -11,13 +17,43 @@ with open('svm_model.pkl', 'rb') as model_file:
 with open('vector_model.pkl', 'rb') as vec_file:
     vectorizer = pickle.load(vec_file)
 
+DB_HOST = "db"
+DB_USER = "root"
+DB_PASSWORD = "root"
+DB_NAME = "Model_Logger"
+def log_to_db(input_params, output, response_time):
+    try:
+        print("Connecting to DB")
+        connection = pymysql.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
+        print("Entering log_to_db")
+        with connection.cursor() as cursor:
+            sql = """
+            INSERT INTO Log (Current_Date_Time, Input_Params, Output, Response_Time)
+            VALUES (%s, %s, %s, %s)
+            """
+            cursor.execute(sql, (datetime.now(), str(input_params), str(output), response_time))
+        connection.commit()
+        print("Data Inserted")
+    finally:
+        connection.close()
+
+
+@app.route('/')
+def home():
+    return send_from_directory('.', 'index.html')  # Serve index.html from the current directory
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         # Get the news text from the request JSON
         data = request.get_json(force=True)
         news_text = data.get('news')
-        print(news_text)
+        start_time = time.time()
         if not news_text:
             return jsonify({'error': 'No news text provided'}), 400
 
@@ -29,18 +65,18 @@ def predict():
 
         # Map prediction to human-readable response
         result = 'Fake News' if prediction[0] == 1 else 'Real News'
+        response_time = time.time() - start_time
 
-        # if prediction[0] == 1:
-        #     result = "Real News"
-        # else:
-        #     result = "Fake News"
-
+        try:
+            log_to_db(input_params=data, output=result, response_time=response_time)
+        except Exception as e:
+            print(f"Logging error: {e}")
         # Return the result as JSON
-        return jsonify({'prediction': result}), 200 
+        return jsonify({'prediction': result}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=50000, debug=True)
